bae6ec2a0072f733a3a369a036947500497debb9
Aksel Dybdal <aksel@ustwo.co.uk>
2012-10-19 14:03:54 +0100

    Initial Commit

---
+++ ADParagraphStyles/HiViewController.h
@@ -0,0 +1,13 @@
+//
+//  HiViewController.h
+//  ADParagraphStyles
+//
+//  Created by Aksel Dybdal on 10/19/12.
+//  Copyright (c) 2012 ustwo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HiViewController : UIViewController
+
+@end
---
+++ ADParagraphStyles/HiViewController.m
@@ -0,0 +1,29 @@
+//
+//  HiViewController.m
+//  ADParagraphStyles
+//
+//  Created by Aksel Dybdal on 10/19/12.
+//  Copyright (c) 2012 ustwo. All rights reserved.
+//
+
+#import "HiViewController.h"
+
+@interface HiViewController ()
+
+@end
+
+@implementation HiViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
