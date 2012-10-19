73d01e67796cd65ad6c4c97ce01e05fdd206d9fb
Aksel Dybdal <aksel@ustwo.co.uk>
2012-10-19 16:57:33 +0100

    Added folder structure

---
+++ Example/HiAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  HiAppDelegate.h
+//  ADParagraphStyles
+//
+//  Created by Aksel Dybdal on 10/19/12.
+//  Copyright (c) 2012 ustwo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class HiViewController;
+
+@interface HiAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) HiViewController *viewController;
+
+@end
---
+++ Example/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ADParagraphStyles
+//
+//  Created by Aksel Dybdal on 10/19/12.
+//  Copyright (c) 2012 ustwo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HiAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HiAppDelegate class]));
+    }
+}
