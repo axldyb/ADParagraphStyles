220a227a179083060e88d947921016370a155c28
Aksel Dybdal <aksel@ustwo.co.uk>
2012-10-26 14:09:06 +0100

    Updated example

--- Example/HiViewController.m
+++ Example/HiViewController.m
@@ -28,7 +28,7 @@
     NSString *stringToDisplay = [self theText];
     
     // Our text view to display text. Can be initialized to prefered size.
-    ADAttributedTextView *textView = [[ADAttributedTextView alloc] initWithFrame:CGRectMake(0, 0, 320, 480) text:stringToDisplay padding:15.0f];
+    ADAttributedTextView *textView = [[ADAttributedTextView alloc] initWithFrame:CGRectMake(0, 0, 320, 700) text:stringToDisplay padding:15.0f];
     
     // Adding background color
     textView.backgroundColor = [UIColor whiteColor];
@@ -40,22 +40,40 @@
     
     // This style is a template style and creates bold text.
     ADParagraphStyle *boldParagrapStyle = [[ADParagraphStyle alloc] initWithParagraphStyleTemplate:kADParagraphStyleBold];
-    boldParagrapStyle.fontName = kADFontPalatinoRoman;
+    boldParagrapStyle.fontName = kADFontPalatinoBold;
     
     // This style is based on the previous style with custom color
     ADParagraphStyle *blueBoldParagraphStyle = [[ADParagraphStyle alloc] initWithStartTag:@"<blue>" endTag:@"</blue>" andBasedOnStyle:boldParagrapStyle];
     blueBoldParagraphStyle.color = [UIColor blueColor];
+    blueBoldParagraphStyle.kerning = 3.0;
     
     // This style has a first line indent
     ADParagraphStyle *lineIndentParagraphStyle = [[ADParagraphStyle alloc] initWithStartTag:@"<in>" endTag:@"</in>" andBasedOnStyle:boldParagrapStyle];
     lineIndentParagraphStyle.firstLineIndent = 15.0f;
+    lineIndentParagraphStyle.spacing = 5.0f;
     lineIndentParagraphStyle.fontSize = 18.0f;
+    
+    // Citation style
+    ADParagraphStyle *citationParagraphStyle = [[ADParagraphStyle alloc] initWithStartTag:@"<citation>" endTag:@"</citation>"];
+    citationParagraphStyle.spacing = 10.0f;
+    citationParagraphStyle.spacingBefore = 10.0f;
+    citationParagraphStyle.firstLineIndent = 20.0f;
+    citationParagraphStyle.headIndent = 20.0f;
+    citationParagraphStyle.tailIndent = 260.0f;
+    citationParagraphStyle.fontSize = 10.0f;
+    citationParagraphStyle.fontName = kADFontPalatinoItalic;
+    
+    // Glyphs
+    ADParagraphStyle *glyphParagraphStyle = [[ADParagraphStyle alloc] initWithStartTag:@"<fi>" endTag:@"</fi>" andBasedOnStyle:mainParagrapStyle];
+    glyphParagraphStyle.glyphs = 5;
     
     // Add our paragraph styles to the text view
     [textView addParagraphStyle:mainParagrapStyle];
     [textView addParagraphStyle:boldParagrapStyle];
     [textView addParagraphStyle:blueBoldParagraphStyle];
     [textView addParagraphStyle:lineIndentParagraphStyle];
+    [textView addParagraphStyle:citationParagraphStyle];
+    [textView addParagraphStyle:glyphParagraphStyle];
     
     // Add text view to our scroll view
     [scrollView addSubview:textView];
@@ -65,7 +83,7 @@
 - (NSString *)theText
 {
     // This is the text we want to display. It has tags similar to HTML used by the paragraph styles to know where to apply themselfs.
-    return @"<text><in>The Macintosh operating system has provided sophisticated text handling and typesetting capabilities from its beginning.</in>\nIn fact, these features sparked the desktop publishing revolution. <bold>Core Text</bold> is the most modern text-handling technology on the platform. It is designed specifically for OS X and is written in C, so it can be called from any language in the system. It is positioned as a core technology to provide consistent, high-performance text services to other frameworks throughout the system, and the Core Text API is accessible to applications that need to use it directly. Core Text resides in the Application Services umbrella framework <blue>(ApplicationServices)</blue> so that it is callable from both Carbon and Cocoa and has all of the lower-level services it needs. <bold>Core Text</bold> is not meant to replace the Cocoa text system, although it provides the underlying implementation for many <bold>Cocoa</bold> text technologies. If you can deal with high-level constructs, such as text views, you can probably use Cocoa. For this reason, Cocoa developers typically have no need to use Core Text directly. Carbon developers, on the other hand, will find Core Textfaster and easier to use, in many cases, than preexisting OS X text layout and font APIs.</text>";
+    return @"<text><in>The Macintosh operating system has provided sophisticated text handling and typesetting capabilities from its beginning.</in>\nIn fact, these features sparked the desktop publishing revolution. <bold>Core Text</bold> is the most modern text-handling technology on the platform. It is designed speci<fi>f</fi>ically for OS X and is written in C, so it can be called from any language in the system. \n<citation>It is positioned as a core technology to provide consistent, high-performance text services to other frameworks throughout the system.</citation>\nThe <bold>Core Text</bold> API is accessible to applications that need to use it directly. <bold>Core Text</bold> resides in the Application Services umbrella framework <blue>(ApplicationServices)</blue> so that it is callable from both Carbon and Cocoa and has all of the lower-level services it needs. <bold>Core Text</bold> is not meant to replace the Cocoa text system, although it provides the underlying implementation for many <bold>Cocoa</bold> text technologies. If you can deal with high-level constructs, such as text views, you can probably use Cocoa. For this reason, Cocoa developers typically have no need to use <bold>Core Text</bold> directly. Carbon developers, on the other hand, will find <bold>Core Text</bold> faster and easier to use, in many cases, than preexisting OS X text layout and font APIs.</text>";
 }
 
 - (void)didReceiveMemoryWarning
